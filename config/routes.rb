14876428176cff4fca7c21b35e9e0abbed9ba4f0
Alex Ponomarev <ikidoit@gmail.com>
2018-03-06 13:55:36 +0300

    added view and styles for sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'application#index'
+  get 'search/related-themes' => 'application#related_themes'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
